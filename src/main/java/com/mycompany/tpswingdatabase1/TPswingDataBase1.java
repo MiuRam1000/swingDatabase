86f1f572f879d77e8b7eda6216081fbd77169284
Andrianantenaina <mioraandriananten@gmail.com>
2025-04-02 20:33:29 +0100

    first commit

---
+++ src/main/java/com/mycompany/tpswingdatabase1/TPswingDataBase1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpswingdatabase1;
+
+/**
+ *
+ * @author Andrianantenaina
+ */
+public class TPswingDataBase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
